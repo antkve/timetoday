0b0141672dbfdad1b31322b8f1460766f1570c31
Anthony Kveder <32168055+antkve@users.noreply.github.com>
2017-09-23 12:31:18 +0000

    Update and rename timetoday.lua to timebar.lua

---
+++ timebar.lua
@@ -0,0 +1,52 @@
+-- Add this part anywhere in your rc.lua {{{
+mytimebar = awful.widget.progressbar()
+mytimebar:set_border_color('#333333')
+mytimebar:set_background_color('#333333')
+mytimebar:set_color('#0000FF')
+mytimebar:set_width(50)
+
+
+mytimebartimer = timer({ timeout = 10 })
+
+mytimebartimer:connect_signal("timeout", function()
+        
+        f = io.popen('/home/anthony/bin/timelog', r)
+        secs = {}
+        i = 0
+        for line in f:lines() do 
+          
+          secs[i] = string.match(line, '%l+: (%d+)')
+          i = i + 1
+        end
+
+        f:close()
+        
+        percentWork = (tonumber(secs[0]) + tonumber(secs[1]) + tonumber(secs[2]))/28800
+        
+        if percentWork < 0.2 then
+          mytimebar:set_color('#FF0000')
+        --elseif percentWork < 0.6 then
+          --mytimebar:set_color('#FFFF00')
+        else
+          mytimebar:set_color('#0000FF')
+        end
+        
+        mytimebar:set_value(percentWork)
+        
+      end)
+mytimebartimer:start()
+mytimebartimer:emit_signal("timeout")
+
+
+
+
+
+
+--- Now add batter
+
+
+
+
+
+
+
